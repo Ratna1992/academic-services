4ce55c434ee3bda426da5e01a7d9bbb86d962156
Ratna1992 <ratnasrinivas1992@gmail.com>
2024-01-06 11:16:15 +0530

    Initial Commit

---
+++ src/test/java/com/academics/AcademicServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
